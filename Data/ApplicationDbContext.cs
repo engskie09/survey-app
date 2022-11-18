3281f15b1eeadcbfb7a192610d7f57adee7b1e12
Rafael Masallo <rafaelmasallo@gmail.com>
2022-11-19 02:47:35 +0800

    ⚙️ setup OnModelCreating on ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using survey_app.Models;
 
 namespace survey_app.Data;
 
 public class ApplicationDbContext: DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){
+    public virtual DbSet<Question> Questions {get; set;}
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){ }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
     }
 }
