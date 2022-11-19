b726b4e93cee67b59dcdef37366f6fcd3ebe4bbf
Rafael Masallo <rafaelmasallo@gmail.com>
2022-11-19 17:16:30 +0800

    ➕ added Create/Edit View and Task operations

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using survey_app.Data;
+using survey_app.Models;
 
 namespace survey_app.Controllers;
 
@@ -19,5 +20,68 @@ public class QuestionController : Controller
         var questions = await _context.Questions.ToListAsync();
 
         return View(questions);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Question question)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Add(question);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            
+            catch (Exception exception)
+            {
+                 ModelState.AddModelError(string.Empty, $"something went wrong {exception.Message}");
+            }
+        }
+
+        ModelState.AddModelError(string.Empty, $"something went wrong invalid model");
+        return View(question);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var exist = await _context.Questions.Where(x => x.Id == id).FirstOrDefaultAsync();
+        return View(exist);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Question question)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                var exist = await _context.Questions.Where(x => x.Id == question.Id).FirstOrDefaultAsync();
+
+                if (exist != null)
+                {
+                    exist.Description = question.Description;
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            
+            catch (Exception exception)
+            {
+                 ModelState.AddModelError(string.Empty, $"something went wrong {exception.Message}");
+            }
+        }
+
+        ModelState.AddModelError(string.Empty, $"something went wrong invalid model");
+        return View(question);
     }
 }
